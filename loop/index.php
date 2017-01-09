d9d1ed0e0934b36922c118dd56f6f225f73cf2f6
Fred <delon.frederic.pro.job@gmail.com>
2017-01-09 10:59:24 +0100

    Exo 1 loop fini

---
+++ loop/index.php
@@ -0,0 +1,15 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>loop</title>
+</head>
+<body>
+	<h1 class="exo1">
+	exo 1 loop
+	<?php  include "../loop/exercice1.php"; ?> 
+	</h1>
+
+</body>
+</html>
