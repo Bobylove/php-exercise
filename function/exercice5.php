22b09fb967107308072d140dfae9cdea801ac3dd
Fred <delon.frederic.pro.job@gmail.com>
2017-01-09 14:11:52 +0100

    exo 5 function fini

---
+++ function/exercice5.php
@@ -0,0 +1,6 @@
+<?php 
+function exo5($chaine1= " le nombre choisi est " ,$nb4 =  4) {
+	echo $chaine1 .  $nb4;
+	return  $chaine1  . $nb4;
+}
+?>
--- function/index.php
+++ function/index.php
@@ -2,6 +2,9 @@
 require __DIR__ . '/../function/exercice2.php';
 require __DIR__ . '/../function/exercice3.php';
 require __DIR__ . '/../function/exercice4.php';
+require __DIR__ . '/../function/exercice5.php';
+require __DIR__ . '/../function/exercice6.php';
+
 ?>
 
 <!DOCTYPE html>
@@ -13,22 +16,38 @@ require __DIR__ . '/../function/exercice4.php';
 <body>
 
 <h1 class="exo1">
+exo1
 <?php exo1(); ?>	
 </h1>
 <br>
 
 <h1 class="exo2">
+exo 2
 <?php exo2(); ?>	
 </h1>
 <br>
 
 <h1 class="exo3">
+exo 3
 <?php exo3(); ?>	
 </h1>
 <br>
 
 <h1 class="exo4">
+exo 4
 <?php exo4(); ?>	
+</h1>
+<br>
+
+<h1 class="exo5">
+exo 5
+<?php exo5(); ?>	
+</h1>
+<br>
+
+<h1 class="exo6">
+exo 6
+<?php exo6(); ?>	
 </h1>
 <br>
 	
