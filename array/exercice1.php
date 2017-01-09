cfd0885c52c2f0a1f37c803bdfdaeffc219eac62
Fred <delon.frederic.pro.job@gmail.com>
2017-01-09 15:48:24 +0100

    exo 1 array fini

---
+++ array/exercice1.php
@@ -0,0 +1,3 @@
+<?php
+$month = array('mois' => " janvier" , " fevrier" , "mars" , "avril", "mai", "juin" , "juillet", "aout" , "septembre","octobre", "novembre", "decembre");
+ ?>
---
+++ array/index.php
@@ -0,0 +1,22 @@
+<?php require __DIR__ . '/../array/exercice1.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>array</title>
+</head>
+<body>
+
+<h1>
+<?php print_r($month) ?>
+</h1>
+<br>
+<!-- 
+<h1>
+
+</h1>
+<br -->
+	
+</body>
+</html>
