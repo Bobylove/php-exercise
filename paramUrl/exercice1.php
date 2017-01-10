7589c043ef6de60a5c12c8925408101879997336
Fred <delon.frederic.pro.job@gmail.com>
2017-01-10 10:34:24 +0100

    exo 1 paramUrl fini

---
+++ paramUrl/exercice1.php
@@ -0,0 +1,8 @@
+<h4>
+<?php
+echo $_GET["nom"]," ";
+echo $_GET["prenom"];
+
+ ?>	
+ <a href="index.php?nom=Nemare&prenom=Jean">home</a>
+</h4>
---
+++ paramUrl/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>parmUrl</title>
+</head>
+<body>
+	
+	<h1 class="exo1">
+		exo 1 : 
+		<a href="exercice1.php?nom=Nemare&prenom=Jean">Exo 1 liens</a>
+	</h1>
+	<br>
+
+
+</body>
+</html>
