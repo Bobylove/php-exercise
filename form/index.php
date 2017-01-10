917d682ef6623a4e9811a37602afde57a7a33856
Fred <delon.frederic.pro.job@gmail.com>
2017-01-10 14:07:41 +0100

    exo 4 form fini

--- form/index.php
+++ form/index.php
@@ -23,10 +23,10 @@
 <h1>EXO 2 post</h1>
 	<form action="user.php" method="post">
 		<label for="nom">nom</label>
-		<input id="nom" type="text" placeholder="dupont">
+		<input  name="nom" id="nom" type="text" placeholder="dupont">
 		<br>
 		<label for="prenom">prenom</label>
-		<input id="prenom" type="text" placeholder="jean">
+		<input name="prenom" id="prenom" type="text" placeholder="jean">
 		<br>
 		<input type="submit">
 	</form>
--- form/user.php
+++ form/user.php
@@ -2,4 +2,11 @@
 $nom = $_GET["nom"]." ";
 $prenom = $_GET["prenom"]. " ";
 ?>
-<h1><?php echo  $nom . $prenom  ;?></h1>
+<h1> exo 3 : <?php echo  $nom . $prenom  ;?></h1>
+
+<?php 
+$nom1 = $_POST["nom"]." ";
+$prenom1 = $_POST["prenom"]. " ";
+
+?>
+<h1> exo 4 : <?php echo  $nom1 . $prenom1  ;?></h1>
