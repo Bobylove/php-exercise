b29418275556f628af908ebc669aa9b3bdc350f6
Fred <delon.frederic.pro.job@gmail.com>
2017-01-10 14:18:59 +0100

    exo 5 form fini

--- form/index.php
+++ form/index.php
@@ -20,18 +20,37 @@
 	<br>
 	<br>
 
-<h1>EXO 2 post</h1>
+	<h1>EXO 2 post</h1>
 	<form action="user.php" method="post">
-		<label for="nom">nom</label>
-		<input  name="nom" id="nom" type="text" placeholder="dupont">
+		<label for="nom1">nom</label>
+		<input  name="nom1" id="nom1" type="text" placeholder="dupont">
 		<br>
-		<label for="prenom">prenom</label>
-		<input name="prenom" id="prenom" type="text" placeholder="jean">
+		<label for="prenom1">prenom</label>
+		<input name="prenom1" id="prenom1" type="text" placeholder="jean">
 		<br>
 		<input type="submit">
 	</form>
 	<br>
 	<br>
+
+	<h1>EXO 5 </h1>
+	<form action="index.php" method="get">
+		<select name="genre" id="genre">
+		<option value="monsieur">Mr</option>
+			<option value="madame">Mme</option>
+		</select>
+		<label for="nom2">nom</label>
+		<input  name="nom2" id="nom2" type="text" placeholder="dupont">
+		<br>
+		<label for="prenom2">prenom</label>
+		<input name="prenom2" id="prenom2" type="text" placeholder="jean">
+		<br>
+		<input type="submit">
+	</form>
+	<br>
+	<br>
+
+
 	
 </body>
 </html>
--- form/user.php
+++ form/user.php
@@ -5,8 +5,19 @@ $prenom = $_GET["prenom"]. " ";
 <h1> exo 3 : <?php echo  $nom . $prenom  ;?></h1>
 
 <?php 
-$nom1 = $_POST["nom"]." ";
-$prenom1 = $_POST["prenom"]. " ";
+$nom1 = $_POST["nom1"]." ";
+$prenom1 = $_POST["prenom1"]. " ";
 
 ?>
 <h1> exo 4 : <?php echo  $nom1 . $prenom1  ;?></h1>
+
+
+<?php 
+$nom2 = $_GET["nom2"]." ";
+$prenom2 = $_GET["prenom2"]. " ";
+$genre2 = $_GET["genre"]. " ";
+
+?>
+<h1> exo 5 : <?php echo $genre2 .  $nom2 . $prenom2  ;?></h1>
+
+
