eee65dc11b4f27e3dc190f08a2b12268be626acc
Fred <delon.frederic.pro.job@gmail.com>
2017-01-10 12:27:24 +0100

    exo 1 form fini

---
+++ form/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Form</title>
+</head>
+<body>
+	<form action="user.php" method="get">
+		<label for="nom">nom</label>
+		<input id="nom" type="text" placeholder="dupont">
+		<br>
+		<label for="prenom">prenom</label>
+		<input id="prenom" type="text" placeholder="jean">
+		<br>
+		<input type="submit">
+	</form>
+
+	
+</body>
+</html>
