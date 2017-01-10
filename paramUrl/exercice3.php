bd1f29e5a66f495824a092544e9c3b9ecdda5324
Fred <delon.frederic.pro.job@gmail.com>
2017-01-10 11:40:16 +0100

    exo 3 paramUrl fini

--- paramUrl/exercice3.php
+++ paramUrl/exercice3.php
@@ -1,13 +1,15 @@
 <?php 
-$gait = $_GET["dateDebut"]["dateFin"];
-if (isset($gait)) {
-	echo " GG wp ";
+$gait = $_GET["dateDebut"];
+$gait2 = $_GET["dateFin"];
+
+if (isset($gait) && isset($gait2)) {
+	echo " Voici les dates :  ";
 foreach ($_GET as $value) {
 	echo $value . " " ;
 }
 	
 }else{
-	echo " loose";
+	echo " je ne trouve pas de dates !  ";
 }
 
 ?>
