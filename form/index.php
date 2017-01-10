fa1d61513136a61b0957eeebe7b7752b05708426
Fred <delon.frederic.pro.job@gmail.com>
2017-01-10 12:29:59 +0100

    exo 2 form fini

--- form/index.php
+++ form/index.php
@@ -5,6 +5,8 @@
 	<title>Form</title>
 </head>
 <body>
+
+	<h1>EXO 1 get</h1>
 	<form action="user.php" method="get">
 		<label for="nom">nom</label>
 		<input id="nom" type="text" placeholder="dupont">
@@ -14,7 +16,21 @@
 		<br>
 		<input type="submit">
 	</form>
+	<br>
+	<br>
 
+<h1>EXO 2 post</h1>
+	<form action="user.php" method="post">
+		<label for="nom">nom</label>
+		<input id="nom" type="text" placeholder="dupont">
+		<br>
+		<label for="prenom">prenom</label>
+		<input id="prenom" type="text" placeholder="jean">
+		<br>
+		<input type="submit">
+	</form>
+	<br>
+	<br>
 	
 </body>
 </html>
