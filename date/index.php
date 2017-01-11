2cf7850cb0c2316904b23f2e610fb79bc15cd326
Fred <delon.frederic.pro.job@gmail.com>
2017-01-11 12:09:42 +0100

    exo 1 date fini

---
+++ date/index.php
@@ -0,0 +1,11 @@
+<?php date_default_timezone_set('UTC'); ?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta charset="UTF-8">
+	<title>Date</title>
+</head>
+<body>
+	<h1> nous somme le  : <?php echo date("j, n, Y");  ?></h1>
+</body>
+</html>
