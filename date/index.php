b880e71639c0b81822c980a9b0f57294e9023f84
Fred <delon.frederic.pro.job@gmail.com>
2017-01-11 12:15:53 +0100

    exo3 date fini

--- date/index.php
+++ date/index.php
@@ -10,5 +10,8 @@
 	<br>
 	<br>
 	<h1> exo 2 nous somme le  : <?php echo date("j, n, y");  ?></h1>
+	<br>
+	<br>
+	<h1> exo 3 nous somme le  : <?php echo date("l, j, F, y");  ?></h1>
 </body>
 </html>
